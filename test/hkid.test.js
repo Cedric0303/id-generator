63c72c70f4f8490a9f0a2166cb52cff815e7244d
Cedric Chen <cedric@lendela.com>
2024-10-24 17:30:05 +0800

    Fix test case

--- test/hkid.test.js
+++ test/hkid.test.js
@@ -1,8 +1,8 @@
 import { test, expect } from 'bun:test';
 import randomHKID from '../src/hkid';
 
-test('should generate a single HKID', () => {
-  const hkid = randomHKID();
+test('should generate a single HKID', async () => {
+  const hkid = await randomHKID();
   expect(hkid.length).toBeOneOf([8, 9]);
   expect(/\d{6,7}/.test(hkid)).toEqual(true);
   expect(/[a-zA-Z]{1,2}/.test(hkid)).toEqual(true);
