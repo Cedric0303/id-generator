8098c725f31f6150f80283b522cfb649c679a1ac
Jun Li Chen <chenjunli7@gmail.com>
2024-11-02 13:34:28 +0800

    Add terminal help menu to SGID generator

--- bin/sgid.js
+++ bin/sgid.js
@@ -1,6 +1,23 @@
 #!/usr/bin/env node
-
+import yargs from 'yargs/yargs';
 import { randomSGID } from '../src/index.js';
 
-const input = process.argv.at(2);
+const argv = await yargs(process.argv.slice(2))
+  .scriptName('sgid')
+  .usage('Usage: $0 <c>')
+  .command('<c>', 'Specify a starting character (optional)')
+  .example([
+    ['$0'],
+    ['$0 S'],
+    ['$0 T'],
+    ['$0 F'],
+    ['$0 G'],
+    ['$0 M'],
+  ])
+  .alias('h', 'help')
+  .alias('v', 'version')
+  .help()
+  .argv;
+
+const input = argv._[0];
 console.log(await randomSGID(input));
